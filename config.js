f43bb6ba95827a0103e77d88e610863c0b45f4b1
Quentin Vansteene <vansteene.quentin@gmail.com>
2023-06-30 11:09:22 +0200

    basic config, custom styles

--- config.js
+++ config.js
@@ -1,1 +1,20 @@
-module.exports = {};
+module.exports = {
+	'colors' : {
+		'mauve'      : '#e2bffd',
+		'aquamarine' : '#90fbff',
+		'blue'       : '#304c7f' 
+	},
+	'primary': 'colors(mauve)',
+	'secondary': 'colors(blue)',
+	'radius': '30px',
+	'body': {
+		'block-background': 'colors(white)',
+	},
+	'link': {
+		'font-color': 'colors(aquamarine)',
+		'font-color-hover': 'colors(aquamarine)',
+		'font-color-focus': 'colors(aquamarine)',
+		'underline': false,
+		'underline-on-hover': true,
+	},
+};
